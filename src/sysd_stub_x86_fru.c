7ea17addc4ba986a6d148c37ad539daa3b4d449b
Dan Cripe <dan.cripe@hpe.com>
2015-09-18 14:43:50 +0000

    Update copright notice (ops-sysd)
    Change-Id: I4a57f05c533a0708b514466e47a4a84cb6e60908
    Signed-off-by: Dan Cripe <dan.cripe@hpe.com>

--- src/sysd_stub_x86_fru.c
+++ src/sysd_stub_x86_fru.c
@@ -1,20 +1,12 @@
-/* vim: ai:ts=4:sw=4:expandtab
- */
 /************************************************************************//**
- * @ingroup sysd
+ * (c) Copyright 2015 Hewlett Packard Enterprise Development LP
  *
- * @file
- * Source for sysd stub generic-x86-64 FRU EEPROM interface.
- *
- * Copyright (C) 2015 Hewlett-Packard Development Company, L.P.
- * All Rights Reserved.
-
  *    Licensed under the Apache License, Version 2.0 (the "License"); you may
  *    not use this file except in compliance with the License. You may obtain
  *    a copy of the License at
-
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
-
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -22,6 +14,11 @@
  *    under the License.
  *
  ***************************************************************************/
+/* @ingroup sysd
+ *
+ * @file
+ * Source for sysd stub generic-x86-64 FRU EEPROM interface.
+ */
 
 #include <stdlib.h>
 #include <string.h>
